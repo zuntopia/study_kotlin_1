baada07981b6b66a689e92e7629db215b29fc8bd
Hyungjun <hjun.rho@samsung.com>
2024-07-14 23:06:55 +0900

    Improve When with test

--- src/main/kotlin/ConditionalWhen.kt
+++ src/main/kotlin/ConditionalWhen.kt
@@ -1,11 +1,18 @@
 package com.study
 
+import RunSwitch
+
 class ConditionalWhen {
     fun runWhenByValue(n: Int){
         when {
             n in 1..3 -> println("number with ${n}")
             else -> println("number with others")
         }
+    }
+
+    fun callSwitchFromJava(n: Int){
+        val rs = RunSwitch()
+        rs.runSwitchByValue(n)
     }
 
 //    fun runSwitchByValue(n: Int){
--- src/test/kotlin/TestConditionalWhen.kt
+++ src/test/kotlin/TestConditionalWhen.kt
@@ -1,11 +1,37 @@
 import com.study.ConditionalWhen
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)// 생명 주기 조절, 디폴트값은 PER_METHOD
 class TestConditionalWhen {
+//    lateinit var c: ConditionalWhen
+//    @BeforeAll
+//    internal fun setUp(){
+//        c = ConditionalWhen()
+//    }
+
+    companion object {
+        lateinit var c: ConditionalWhen
+        @JvmStatic
+        @BeforeAll
+        internal fun setUp(){
+            c = ConditionalWhen()
+        }
+    }
+
     @Test
+    @DisplayName("When")
     fun testRunWhen(){
-        val c = ConditionalWhen()
         c.runWhenByValue(1)
         c.runWhenByValue(10)
+    }
+
+    @Test
+    @DisplayName("Switch")
+    fun testRunSwitchFromJava(){
+        c.callSwitchFromJava(1)
+        c.callSwitchFromJava(5)
     }
 }
